420858c78d6016576a4a70553284c1b16785ca7f
Shahan Khan <contact@shahan.me>
2014-09-16 16:19:33 -0700

    Initial framework setup

---
+++ TurboApi/TurboApi.h
@@ -0,0 +1,19 @@
+//
+//  TurboApi.h
+//  TurboApi
+//
+//  Created by Shahan Khan on 9/16/14.
+//  Copyright (c) 2014 Shahan Khan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TurboApi.
+FOUNDATION_EXPORT double TurboApiVersionNumber;
+
+//! Project version string for TurboApi.
+FOUNDATION_EXPORT const unsigned char TurboApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TurboApi/PublicHeader.h>
+
+
